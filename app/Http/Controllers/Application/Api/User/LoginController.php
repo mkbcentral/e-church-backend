b1921dfeced85b142a8c639290112a66806c0897
mkbcentral <mkbcentralcode@gmail.com>
2024-03-20 12:20:49 +0100

    Login functionnality

--- app/Http/Controllers/Application/Api/User/LoginController.php
+++ app/Http/Controllers/Application/Api/User/LoginController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Application\Api\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class LoginController extends Controller
 {
@@ -12,6 +15,29 @@ class LoginController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+            $user = User::where('email', $request->email)->first();
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response([
+                    'message' => 'The provided credentials are incorrect.',
+                ], 401);
+            } else {
+                $token = $user->createToken('token')->plainTextToken;
+                return response([
+                    'user' => $user,
+                    'token' => $token,
+                    'message' => 'Connexion avec succès.',
+                ], 201);
+            }
+        } catch (HttpException $ex) {
+            return response([
+                'error' => $ex->getMessage(),
+                'code' => $ex->getStatusCode(),
+            ]);
+        }
     }
 }
