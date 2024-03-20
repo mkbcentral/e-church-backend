d070cf39941051ec51a7f4184f79f622979ba5c7
mkbcentral <mkbcentralcode@gmail.com>
2024-03-20 12:28:20 +0100

    Make register controller functionnality

--- app/Http/Controllers/Application/Api/User/RegisterController.php
+++ app/Http/Controllers/Application/Api/User/RegisterController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Application\Api\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class RegisterController extends Controller
 {
@@ -12,6 +15,28 @@ class RegisterController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required',
+            ]);
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+            $token = $user->createToken('token')->plainTextToken;
+            return response([
+                'user' => $user,
+                'token' => $token,
+                'message' => 'Inscription avec succès.',
+            ], 201);
+        } catch (HttpException $ex) {
+            return response([
+                'error' => $ex->getMessage(),
+                'code' => $ex->getStatusCode(),
+            ]);
+        }
     }
 }
