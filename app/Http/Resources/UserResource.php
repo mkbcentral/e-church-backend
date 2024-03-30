9f7dbf0113d0fc7b09a20f275d7898c97fd7e9af
mkbcentral <mkbcentralcode@gmail.com>
2024-03-30 20:56:40 +0100

    Fix load image url on dev et pro mode

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,12 +15,22 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return  [
-            'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
-            'phone' => $this->phone,
-            'avatar' => $this->avatar ? config('app.url') . '/storage/' . $this->avatar : null,
-        ];
+        if (config('app.env') == 'local') {
+            return  [
+                'id' => $this->id,
+                'name' => $this->name,
+                'email' => $this->email,
+                'phone' => $this->phone,
+                'avatar' => $this->avatar ? config('app.url') . '/storage/' . $this->avatar : null,
+            ];
+        } else {
+            return  [
+                'id' => $this->id,
+                'name' => $this->name,
+                'email' => $this->email,
+                'phone' => $this->phone,
+                'avatar' => $this->avatar ? config('app.url') . '/public/storage/' . $this->avatar : null,
+            ];
+        }
     }
 }
